0af116d0e4efbbfd9924922f52739e0c77a7ca57
Kricheli <krichelieden@gmail.com>
2023-06-20 21:36:18 +0300

    adding images and setting the bg

---
+++ app.js
@@ -0,0 +1,58 @@
+const cardsArray = [
+  {
+    name: 'ross_and_rachel',
+    Image: 'media/friends_rachel.jpg',
+  },
+  {
+    name: 'ross_and_rachel',
+    Image: 'media/ross.jpg',
+  },
+  {
+    name: 'monica_and_chandler',
+    Image: 'media/friends_monica.jpg',
+  },
+  {
+    name: 'monica_and_chandler',
+    Image: 'media/friends_chandler.jpg',
+  },
+  {
+    name: 'pam_and_jim',
+    Image: 'media/the_office_pam.jpg',
+  },
+  {
+    name: 'pam_and_jim',
+    Image: 'media/the_office_jim.jpg',
+  },
+  {
+    name: 'mitch_and_cam',
+    Image: 'media/modern_family_mitchell.jpg',
+  },
+  {
+    name: 'mitch_and_cam',
+    Image: 'media/modern_family_cam.jpg',
+  },
+  {
+    name: 'lily_and_marshell',
+    Image: 'media/himym_lily.jpg',
+  },
+  {
+    name: 'lily_and_marshel',
+    Image: 'media/himym_marshell.jpg',
+  },
+  {
+    name: 'the_simpons',
+    Image: 'media/the_simposons_homer.jpg',
+  },
+  {
+    name: 'the_simpsons',
+    Image: 'media/the_simpsons_marge.jpg',
+  },
+]
+
+const card = document.querySelector('.card')
+card.addEventListener('click', flippedCard)
+
+const flippedCard=()=>{
+  
+}
+
